bbb80212a0d428e02dd789ee262fce602ce5c0e2
shiro1702 <shiro1702@gmail.com>
2021-06-04 20:45:04 +0700

    add new date gen

--- dist/_nuxt/static/1622563437/2021-05/15/edit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622563437",layout:"default",error:a,serverRendered:true,routePath:"\u002F2021-05\u002F15\u002Fedit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
