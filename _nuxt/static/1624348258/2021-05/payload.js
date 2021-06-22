f657e6588f9ebced71f7310a671b1854a810c128
shiro1702 <shiro1702@gmail.com>
2021-06-22 14:51:52 +0700

    Updates

---
+++ _nuxt/static/1624348258/2021-05/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/2021-05", {data:[{},{tracked:null,noInfo:false}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1624348258/2021-06/13/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/2021-06/13", {data:[{},{dayInfo:{},noInfo:true}],fetch:{},mutations:void 0});
