ea919735c12ec77ac310adee368da77c5770cc83
shiro1702 <shiro1702@gmail.com>
2021-08-02 15:14:36 +0700

    add new info

---
+++ dist/_nuxt/static/1627892035/2021-08/06/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627892035",layout:"default",error:a,serverRendered:true,routePath:"\u002F2021-08\u002F06",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
